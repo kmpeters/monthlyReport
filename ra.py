560180d32fd0f282b91f67011fd1703a84193fb7
Kevin Peterson <kmpeters@anl.gov>
2014-12-11 17:01:45 -0600

    Added option to show WBS codes in wt output

--- ra.py
+++ ra.py
@@ -34,6 +34,10 @@ class MyCli(cli.ReportCli):
     #!self.showCorrectDefaults = True
     #!self.showCorrectDescLen = 60
 
+    # Improve the wt command for Dayforce
+    #!self.showCostCodes = True
+    self.showWBSCodes = True
+
   # Override the createReportLog function so that MyReportLog is used instead of ReportLog
   def createReportLog(self, filepath):
     return MyReportLog(filepath)
